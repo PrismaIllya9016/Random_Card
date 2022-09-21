c4c01be7e01d34847e7c757efaf83d52f2bb8f22
moniasuka <moniasuka@hotmail.com>
2022-09-21 03:30:22 +0000

    push ejercicio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let type = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let suitecolorlist = ["#ff0000", "#000000"];
+  let suitecolor =
+    suitecolorlist[Math.floor(Math.random() * suitecolorlist.length)];
+
+  let textoh1 = document.querySelector("#generador1");
+  let aux1 = "";
+  aux1 = "<p>" + type[Math.floor(Math.random() * type.length)] + "</p>";
+  textoh1.innerHTML = aux1;
+  textoh1.style.color = suitecolor;
+  console.log(textoh1);
+
+  let textoh2 = document.querySelector("#generador2");
+  let aux2 = "";
+  aux2 = "<p>" + number[Math.floor(Math.random() * number.length)] + "</p>";
+  textoh2.innerHTML = aux2;
+  console.log(textoh2);
+
+  let textoh3 = document.querySelector("#generador3");
+  let aux3 = aux1;
+  textoh3.innerHTML = aux3;
+  textoh3.style.color = suitecolor;
+  console.log(textoh3);
 };
